39e72e1f352f90a483e468b19d39229bb936dbf1
Francesco Aiello <aiellof@oregonstate.edu>
2017-10-06 15:26:02 -0700

    Circles bounce off the walls and change to a random color every time they bounce.

--- sketch.js
+++ sketch.js
@@ -1,13 +1,32 @@
 // Zombulator by Francesco Aiello
 
+var zombieX = 80;
+var zombie2X = 100;
+var change = 5;
+var randomnumber1 = 120;
+var randomnumber2 = 35;
+var randomnumber3 = 16;
+
 function setup() {
 	createCanvas(800, 800);
 }
 
 
 function draw() {
-	fill(255, 0, 0);
-	ellipse(50, 50, 80, 80);
-	fill(150, 150, 200);
-	ellipse(200, 100, 80, 80);
-}
+	background(255, 255, 255);
+	fill(randomnumber1, randomnumber2, randomnumber3);
+	ellipse(zombieX, 50, 80, 80);
+	fill(randomnumber2, randomnumber3, randomnumber1);
+	ellipse(zombie2X, 150, 80, 80);
+	zombieX = zombieX + change;
+	zombie2X = zombie2X + change;
+
+	if (zombieX >= 800 || zombieX <=0) {
+		change = change * -1;
+		randomnumber = Math.floor((Math.random() * (256)) + 1);
+		randomnumber2 = Math.floor((Math.random() * (256)) + 1);
+		randomnumber3 = Math.floor((Math.random() * (256)) + 1);
+		}
+	}
+
+
